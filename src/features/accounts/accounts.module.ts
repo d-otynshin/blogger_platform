9e24d426743de7a53a5fa4c1d58d9dfe449adc8f
d-otynshin <daniyar.otynshin@gmail.com>
2025-01-18 22:18:44 +0500

    fix

--- src/features/accounts/accounts.module.ts
+++ src/features/accounts/accounts.module.ts
@@ -68,7 +68,7 @@ const guards = [
       useFactory: (): JwtService => {
         return new JwtService({
           secret: process.env.ACCESS_TOKEN_SECRET,
-          signOptions: { expiresIn: '1000s' },
+          signOptions: { expiresIn: '10s' },
         });
       },
       inject: [
@@ -80,7 +80,7 @@ const guards = [
       useFactory: (): JwtService => {
         return new JwtService({
           secret: process.env.REFRESH_TOKEN_SECRET, //TODO: move to env. will be in the following lessons
-          signOptions: { expiresIn: '2000s' },
+          signOptions: { expiresIn: '20s' },
         });
       },
       inject: [
