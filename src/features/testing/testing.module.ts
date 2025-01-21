e47ff67513e21b652fddf3bb24166abfb44c3bdf
d-otynshin <daniyar.otynshin@gmail.com>
2025-01-21 12:20:58 +0500

    fix: provide repository to testing module

--- src/features/testing/testing.module.ts
+++ src/features/testing/testing.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { TestingController } from './testing.controller';
 import { AccountsModule } from '../accounts/accounts.module';
 import { UsersPostgresqlRepository } from '../accounts/infrastructure/repositories/users-postgresql.repository';
+import { SecurityPostgresqlRepository } from '../accounts/infrastructure/repositories/security-postgresql.repository';
 // import { PlatformModule } from '../platform/platform.module';
 
 @Module({
   imports: [AccountsModule], // PlatformModule
   controllers: [TestingController],
-  // TODO: should i provide?
-  providers: [UsersPostgresqlRepository],
+  providers: [UsersPostgresqlRepository, SecurityPostgresqlRepository],
 })
 export class TestingModule {}
