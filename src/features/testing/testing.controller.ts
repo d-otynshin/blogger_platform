8a7731302b7dd398cebd1dabc6fa3e939aab5984
d-otynshin <daniyar.otynshin@gmail.com>
2025-01-22 14:31:31 +0500

    fix

--- src/features/testing/testing.controller.ts
+++ src/features/testing/testing.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Delete, HttpCode, HttpStatus } from '@nestjs/common';
 import { UsersSQLRepository } from '../accounts/infrastructure/repositories/users-sql.repository';
 import { SecurityPostgresqlRepository } from '../accounts/infrastructure/repositories/security-postgresql.repository';
+import { CommentsSQLRepository } from '../platform/infrastructure/repositories/comments-sql.repository';
+import { BlogsSQLRepository } from '../platform/infrastructure/repositories/blogs-sql.repository';
+import { PostsSQLRepository } from '../platform/infrastructure/repositories/posts-sql.repository';
 
 @Controller('testing')
 export class TestingController {
   constructor(
     private usersRepository: UsersSQLRepository,
+    private blogsRepository: BlogsSQLRepository,
+    private postsRepository: PostsSQLRepository,
+    private commentsRepository: CommentsSQLRepository,
     private securityRepository: SecurityPostgresqlRepository,
   ) {}
 
@@ -15,10 +21,9 @@ export class TestingController {
     await Promise.all([
       await this.usersRepository.deleteAll(),
       await this.securityRepository.deleteAll(),
-      // await this.UserModel.deleteMany(),
-      // await this.BlogModel.deleteMany(),
-      // await this.PostModel.deleteMany(),
-      // await this.CommentModel.deleteMany(),
+      await this.blogsRepository.deleteAll(),
+      await this.postsRepository.deleteAll(),
+      await this.commentsRepository.deleteAll(),
     ]);
   }
 }
