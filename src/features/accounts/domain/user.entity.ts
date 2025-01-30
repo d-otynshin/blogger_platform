0654b1720291e6410c805bc8b5cef0cbf2f4adc4
d-otynshin <daniyar.otynshin@gmail.com>
2025-01-30 13:30:39 +0500

    fix

--- src/features/accounts/domain/user.entity.ts
+++ src/features/accounts/domain/user.entity.ts
@@ -2,8 +2,9 @@ import {
   Entity,
   Column,
   CreateDateColumn,
-  PrimaryGeneratedColumn,
+  PrimaryGeneratedColumn, OneToMany
 } from 'typeorm';
+import { PostsInteraction } from '../../platform/domain/posts-interaction.entity';
 
 @Entity('users')
 export class User {
@@ -24,6 +25,9 @@ export class User {
 
   @Column({ default: false })
   is_confirmed: boolean;
+
+  @OneToMany(() => PostsInteraction, (interaction) => interaction.user)
+  interactions: PostsInteraction[];
 
   @CreateDateColumn({ type: 'timestamp with time zone' })
   created_at: Date;
