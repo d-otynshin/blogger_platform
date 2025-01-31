33b7fc9273b2b9acfdbda689366d595b9e64fa3e
d-otynshin <daniyar.otynshin@gmail.com>
2025-01-31 19:56:41 +0500

    add interactions to comments

--- src/features/platform/domain/comment.entity.ts
+++ src/features/platform/domain/comment.entity.ts
@@ -1,13 +1,15 @@
 import {
   Entity,
-  PrimaryGeneratedColumn,
   Column,
-  CreateDateColumn,
+  OneToMany,
   ManyToOne,
   JoinColumn,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
 } from 'typeorm';
 import { User } from '../../accounts/domain/user.entity';
 import { Post } from './post.entity';
+import { CommentsInteraction } from './comments-interaction.entity';
 
 @Entity('comments')
 export class Comment {
@@ -27,4 +29,7 @@ export class Comment {
   @ManyToOne(() => Post, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'post_id' })
   post: Post;
+
+  @OneToMany(() => CommentsInteraction, (interaction) => interaction.comment)
+  interactions: CommentsInteraction[];
 }
