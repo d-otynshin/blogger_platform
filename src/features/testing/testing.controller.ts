54a542f625e2cf026a8f302bd447dcd01fb7cbe8
d-otynshin <daniyar.otynshin@gmail.com>
2025-02-25 13:45:59 +0500

    rollback testing

--- src/features/testing/testing.controller.ts
+++ src/features/testing/testing.controller.ts
@@ -1,38 +1,38 @@
 import { Controller, Delete, HttpCode, HttpStatus } from '@nestjs/common';
-// import { UsersRepository } from '../accounts/infrastructure/repositories/users.repository';
+import { UsersRepository } from '../accounts/infrastructure/repositories/users.repository';
 import { SecurityRepository } from '../accounts/infrastructure/repositories/security.repository';
 import { CommentsRepository } from '../platform/infrastructure/repositories/comments.repository';
 import { BlogsRepository } from '../platform/infrastructure/repositories/blogs.repository';
 import { PostsRepository } from '../platform/infrastructure/repositories/posts.repository';
-// import { QuestionsRepository } from '../quiz/infrastructure/repositories/qustions.repository';
-// import { GamesRepository } from '../quiz/infrastructure/repositories/games.repository';
-// import { QuizRepository } from '../quiz/infrastructure/repositories/quiz.repository';
+import { QuestionsRepository } from '../quiz/infrastructure/repositories/qustions.repository';
+import { GamesRepository } from '../quiz/infrastructure/repositories/games.repository';
+import { QuizRepository } from '../quiz/infrastructure/repositories/quiz.repository';
 
 @Controller('testing')
 export class TestingController {
   constructor(
-    // private quizRepository: QuizRepository,
-    // private usersRepository: UsersRepository,
+    private quizRepository: QuizRepository,
+    private usersRepository: UsersRepository,
     private blogsRepository: BlogsRepository,
     private postsRepository: PostsRepository,
-    // private gamesRepository: GamesRepository,
+    private gamesRepository: GamesRepository,
     private commentsRepository: CommentsRepository,
     private securityRepository: SecurityRepository,
-    // private questionsRepository: QuestionsRepository,
+    private questionsRepository: QuestionsRepository,
   ) {}
 
   @Delete('all-data')
   @HttpCode(HttpStatus.NO_CONTENT)
   async deleteAll() {
     await Promise.all([
-      // await this.usersRepository.deleteAll(),
+      await this.usersRepository.deleteAll(),
       await this.blogsRepository.deleteAll(),
       await this.postsRepository.deleteAll(),
-      // await this.gamesRepository.deleteAll(),
-      // await this.quizRepository.deleteAll(),
+      await this.gamesRepository.deleteAll(),
+      await this.quizRepository.deleteAll(),
       await this.securityRepository.deleteAll(),
       await this.commentsRepository.deleteAll(),
-      // await this.questionsRepository.deleteAll(),
+      await this.questionsRepository.deleteAll(),
     ]);
   }
 }
